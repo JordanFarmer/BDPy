2d9f3499169d05e4e8ce5869b7a7a3a871fa5987
kevin <kevin.chuang@megabank.com.tw>
2018-09-03 17:01:45 +0800

    c

---
+++ py27_lab96.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection1 = sqlite3.connect('.\\db\\lab95.sqlite')
+
+employees = [{'NAME': 'Mark', 'Age': 43, 'DEPT': 1, 'ADDR': 'Hsinchu'},
+             {'NAME': 'Tim', 'Age': 42, 'DEPT': 3, 'ADDR': 'Taipei'},
+             {'NAME': 'Ken', 'Age': 41, 'DEPT': 1, 'ADDR': 'Taichung'},
+             {'NAME': 'John', 'Age': 4, 'DEPT': 3, 'ADDR': 'Taipei'}]
+sql_insert_dml = 'INSERT INTO EMPLOYEE(NAME, AGE, DEPT, ADDRESS) VALUES(?,?,?,?)'
+for e in employees:
+    connection1.execute(sql_insert_dml, (e['NAME'], e['Age'], e['DEPT'], e['ADDR']))
+
+connection1.commit()
+connection1.close()
